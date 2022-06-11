e4483e449bb592d530370f02a60102b7fd6f9c30
Astra Berry <astraberry82@gmail.com>
2022-06-11 21:14:36 +0000

    Added comments

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
+
+#establishing first and second inputs 0 = first digit and 1 = second digit
 first_digit = int(two_digit_number[0])
 second_digit = int(two_digit_number[1])
 
+#two_digit_number will equal first + second digits
 two_digit_number = first_digit + second_digit
 print(two_digit_number)
 
